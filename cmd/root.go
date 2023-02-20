f8b5442f2a204533ae9880801e5d9f5623b403c5
YJ-Arch <yj1516268@outlook.com>
2023-02-20 16:30:40 +0800

    scleaner独立到库

---
+++ cmd/root.go
@@ -0,0 +1,54 @@
+/*
+File: root.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-02-20 16:15:52
+
+Description: 程序未带子命令或参数时执行
+*/
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// 在没有任何子命令的情况下调用时的基本命令
+var rootCmd = &cobra.Command{
+	Use:   "scleaner",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.scleaner.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
