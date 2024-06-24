c3219252fe3dc5c31d6ef277ea92464c7b7f3f51
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2024-06-24 11:17:21 +0800

    v0.6.9

--- general/version.go
+++ general/version.go
@@ -16,7 +16,7 @@ import (
 
 const (
 	Name    string = "Scleaner"                 // 程序名
-	Version string = "v0.6.8"                   // 程序版本
+	Version string = "v0.6.9"                   // 程序版本
 	Project string = "github.com/yhyj/scleaner" // 项目地址
 )
 
